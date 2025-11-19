1462e3545f88e585799d62493b122b1f615455a5
Cloaked-Walker <gouthama921@gmail.com>
2025-11-19 13:30:09 +0530

    added fetch&header

---
+++ src/components/Search.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import search from '../assets/search.svg'
+
+const Search = ({searchTerm, setSerchTerm}) => {
+  return (
+    <div className='search'>
+      <div>
+        <img src={search} alt="search" />
+
+        <input 
+          type='text'
+          placeholder='Searchthrough thousands of movies'
+          value={searchTerm}
+          onChange={(e) => setSerchTerm(e.target.value)}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default Search
